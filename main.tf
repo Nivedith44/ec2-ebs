e954d61419d5b5272160090e2daa4dc3952d7e92
niveditha44 <ailneniniveditha8@gmail.com>
2024-04-15 01:17:37 +0530

    first commit

--- main.tf
+++ main.tf
@@ -14,14 +14,10 @@ resource "aws_ebs_volume" "vol1" {
   count = 1
   size = 1
 }
-#resource "aws_volume_attachment" "nive1-to-vol1" {
-    #count        = "${var.instance_count}"
-    #device_name = "/dev/sdc"
-    #volume_id = aws_ebs_volume.vol1[count.index].id
-    #instance_id = aws_instance.nive1[index.index].id
-#}
-#resource "aws_volume_attachment" "nive1-to-vol1" {
-  #device_name = "/dev/sdc"
-  #volume_id = aws_ebs_volume.vol1.id
-  #instance_id = i-0a6812e4220c71622.id
-#}
+resource "aws_volume_attachment" "nive1-to-vol1" {
+    count        = "${var.instance_count}"
+    device_name = "/dev/sdc"
+    volume_id = aws_ebs_volume.vol1[count.index].id
+    instance_id = aws_instance.nive1[count.index].id
+}
+
